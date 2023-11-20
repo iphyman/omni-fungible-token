b37f716f4e4e284147a45fcdaab69e73a957f3f0
Emmanuel <opensoftweb@gmail.com>
2023-11-20 21:11:32 +0100

    feat: add lib to convert b/w evm address to bytes32

--- src/libraries/AddressTypeCast.sol
+++ src/libraries/AddressTypeCast.sol
@@ -2,4 +2,16 @@
 
 pragma solidity ^0.8.19;
 
-library AddressTypeCast {}
+library AddressTypeCast {
+    error InvalidEVMAddress(bytes32 account);
+
+    function addressToBytes32(address account) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(account)));
+    }
+
+    function bytes32ToAddress(bytes32 account) internal pure returns (address) {
+        if (uint256(account) >> 160 != 0) revert InvalidEVMAddress(account);
+
+        return address(uint160(uint256(account)));
+    }
+}
