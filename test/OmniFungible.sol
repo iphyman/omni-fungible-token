cb86a761d59d8b4dc8cfefca65173f44a7b57d77
Emmanuel <opensoftweb@gmail.com>
2023-11-24 11:41:55 +0100

    feat: begin testing

---
+++ test/OmniFungible.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Test, console2 } from "forge-std/Test.sol";
+import { OmniFungible } from "../src/OmniFungible.sol";
+
+contract OmniFungibleTest is Test {
+    OmniFungible public omniFungible;
+
+    function setUp() public { }
+}
